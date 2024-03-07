21c7c24176af51babf14f04f3073702b72b9f982
macdonald34 <+>
2024-03-07 13:43:45 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if self.is_anagram(anagram)]
+
+    def is_anagram(self, candidate):
+        return sorted(candidate.lower()) == sorted(self.word.lower())
